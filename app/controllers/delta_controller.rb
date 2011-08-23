b0bd74e6682de995bbc13e1b037f02735599b530
Dave Schukin <dave@schukin.com>
2011-08-23 01:49:16 -0400

    [DeltaController] Added alias_method_chain example

--- app/controllers/delta_controller.rb
+++ app/controllers/delta_controller.rb
@@ -1,9 +1,16 @@
 class DeltaController < ApplicationController
 
+
 	def home
 		@value = 3
 		@string = stringify
-		@string2 = DeltaController.hello
+		@string2 = some_method
+		@string3 = some_method_without_awesomeness
+		@string4 = DeltaController.another_method
+	end
+
+	def some_method
+		"DeltaController's default implementation of some_method"
 	end
 
 end
--- lib/schukin/ultra_stringifier.rb
+++ lib/schukin/ultra_stringifier.rb
@@ -3,6 +3,12 @@ module Schukin
 		def self.included(base)
 			base.send(:include, InstanceMethods)
 			base.send(:extend, ClassMethods)
+
+			# alias_method_chain does some cool stuff:
+			#	We redefine the default 'some_method' to use the definition in
+			#	'some_method_with_awesomeness'. We can still use the old method definition
+			#	by invoking 'some_method_without_awesomeness'.
+			base.alias_method_chain :some_method, :awesomeness
 		end
 
 		module InstanceMethods
@@ -15,14 +21,19 @@ module Schukin
 					"Three"
 				end
 			end
+
+			def some_method_with_awesomeness
+				"This was overridden by UltraStringifier"
+			end
 		end
 
 		module ClassMethods
 			# Note that this is method's signature is
-			#	def hello
+			#	def another_method
 			# and not
-			#	def self.hello
-			def hello
+			#	def self.another_method
+			# even though it will be added as a class method
+			def another_method
 				"Hello world!"
 			end
 		end
